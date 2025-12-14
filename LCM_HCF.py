9c8f26e83d6aff02e37b54673d05057ff772caf7
venkatbommidi23 <128401434+venkatbommidi23@users.noreply.github.com>
2025-12-14 15:03:47 +0530

    Create LCM_HCF.py

---
+++ LCM_HCF.py
@@ -0,0 +1,18 @@
+#We can do using prime factorization but 
+#Time -O(sqrt(a)+sqrt(b))--SLow for big numbers
+#Space-O(no of prime factors)
+
+
+#HCF is calculated using Euclidean algorithm; LCM is derived using LCM = (a×b)/HCF.
+
+class Solution:
+    def lcmAndGcd(self, a : int, b : int) -> List[int]:
+        def gcd(a,b):
+            while(b):
+               a,b=b,a%b
+            return a
+        hcf=gcd(a,b)
+        return [a*b//hcf,hcf]
+    
+    #Time-O(log(min(a,b)))---O(logn)
+    #Space-O(1)
