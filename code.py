02470f05443769d7ac5637f3553891b3c716c7c6
venkatbommidi23 <128401434+venkatbommidi23@users.noreply.github.com>
2025-12-14 10:05:16 +0530

    Update and rename code.py to prime_number.py

--- code.py
+++
@@ -1,2 +0,0 @@
-def code():
-print("Hello world")
---
+++ prime_number.py
@@ -0,0 +1,19 @@
+import math
+class Solution:
+    def isPrime(self, n):
+        if n<2:
+            return False
+        if n==2:
+            return True
+        if n%2==0:
+            return False
+  
+        #Why only upto sqrt(n) because any non-prime number will have factor <=sqrt(n)
+        #eg:n=36 (1,36) (2,18) (3,12) (4,9) (6,6) after this the factors start repeating
+        #a*b one is <=sqrt(n) and other is >=sqrt(n)
+      
+        sqrt=int(math.sqrt(n))
+        for i in range(3,sqrt+1,2):
+            if n%i==0:
+                return False
+        return True
